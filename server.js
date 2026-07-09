1774703c80480f0b204d4e0b143d10b04d996322
Zuhair05 <alshabaq98@gmail.com>
2026-07-09 12:14:05 +0300

    Creat a model

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+const dotenv = require('dotenv').config(); // making .env file available to the app
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(morgan('dev'));
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
 app.get('/', async (req, res) => {
-  res.send('Hello, World!');
+  res.render('home.ejs');
 });
 
 app.listen(3000, () => {
