50d9893ffaf585c7e88fff0194225f92e60a9fe5
Zuhair05 <alshabaq98@gmail.com>
2026-07-09 12:44:32 +0300

    Creat a fruits a send it to the browser

--- server.js
+++ server.js
@@ -15,11 +15,33 @@ mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
+const Fruit = require('./models/fruit.js');
+
 
 app.get('/', async (req, res) => {
   res.render('home.ejs');
 });
 
+// this route will change often 
+app.get('/fruits', async (req, res) => {
+    //creat a fruit object 
+    const fruitData = {}
+    fruitData.name = 'mango'
+    fruitData.isReadyToEat = false
+    //use a  mongoose method to add it to the DB 
+    let createdFruit = await Fruit.create(fruitData)
+    //view the created fruit 
+    res.send(createdFruit)
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+//code graveyard  ======================================================
+//creat a fruit object 
+    // const fruitData = {}
+    // fruitData.name = 'mango'
+    // fruitData.isReadyToEat = false
+    //use a  mongoose method to add it to the DB 
+    // let createdFruit = await Fruit.create(fruitData)
